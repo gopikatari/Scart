fcbbe4d2a529b3ccc62e3686e09a39ae633c0060
gopikatari <katarigopi@gmail.com>
2016-11-16 17:58:04 +0530

    cmt1

---
+++ ShoppingCart/Controllers/StoreFrontController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCartApp.DAL;
+using ShoppingCartApp.Models;
+
+namespace ShoppingCartApp.Controllers
+{
+    public class StoreFrontController : Controller
+    {
+
+        private readonly MvcAffableBeanContext db = new MvcAffableBeanContext();
+        // GET: StoreFront
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+    }
+}
---
+++ ShoppingCart/DAL/MvcAffableBeanContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ShoppingCartApp.Models;
+
+namespace ShoppingCartApp.DAL
+{
+    public class MvcAffableBeanContext:DbContext
+
+    {
+        public MvcAffableBeanContext() : base("MvcAffableBean")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<CustomerOrder> CustomerOrders { get; set; }
+
+        public DbSet<OrderedProduct> Orderedproducts { get; set; }
+
+        public DbSet<Cart> Carts { get; set; }
+        
+
+        public System.Data.Entity.DbSet<ShoppingCartApp.Models.RegisterModel> RegisterModels { get; set; }
+    }
+}
